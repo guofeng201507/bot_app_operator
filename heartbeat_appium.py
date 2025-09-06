5bd3993a2b516e68544b9485b5c72a707652d93a
guofe <guofeng20094@gmail.com>
2025-09-06 22:42:53 +0800

    add heartbeat check

--- heartbeat_appium.py
+++ heartbeat_appium.py
@@ -1,30 +1,18 @@
-# from appium import webdriver
-#
-# caps = {
-#     "platformName": "Android",
-#     "automationName": "UiAutomator2",
-#     "udid": "121.43.57.95:100",
-#     "newCommandTimeout": 300,
-# }
-#
-# driver = webdriver.Remote(
-#     command_executor="http://127.0.0.1:4723/wd/hub",
-#     desired_capabilities=caps
-#
-# )
-
-# print("session:", driver.session_id)
-# driver.quit()
-
-#
+from dotenv import load_dotenv
 from appium import webdriver
 from appium.options.android.uiautomator2.base import UiAutomator2Options
+import os
+
+# ---------- 环境 ----------
+load_dotenv()
+ADB = os.getenv("ADB_HOST_PORT")
+APPIUM = os.getenv("APPIUM_ENDPOINT", "http://127.0.0.1:4723")
 
 # 你的能力
 caps = {
     "platformName": "Android",
     "automationName": "UiAutomator2",
-    "udid": "121.43.57.95:100",  # 云手机 ADB 地址
+    "udid": ADB,  # 云手机 ADB 地址
     # "app": r"C:\path\to\your.apk",       # 二选一：给 app 路径
     # "appPackage": "com.demo.app",        # 或者给包名 + Activity
     # "appActivity": ".MainActivity",
@@ -38,6 +26,6 @@ caps = {
 options = UiAutomator2Options().load_capabilities(caps)
 
 # 注意：新版本 Appium 不要求 /wd/hub，两个都能用
-driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+driver = webdriver.Remote(APPIUM, options=options)
 print("session:", driver.session_id)
 driver.quit()
